e96ef16c0a999dcb60d51da44e92c087fca25607
Besitzer <sirenar3171@gmail.com>
2025-04-03 21:43:36 +0200

    Exception Commit

--- src/main/java/bg/softuni/bookrestservice/web/GlobalExceptionHandler.java
+++ src/main/java/bg/softuni/bookrestservice/web/GlobalExceptionHandler.java
@@ -7,11 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ReviewNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleReviewNotFoundException(ReviewNotFoundException exception) {
 
